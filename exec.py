2000520831e6b5d6ec1b37c9569d03b74c7afbd9
malandr <quadr988@gmail.com>
2022-10-06 18:51:13 +0300

    init

--- exec.py
+++ exec.py
@@ -5,11 +5,16 @@ def week_number_of_month(date_value):
     return (date_value.isocalendar()[1] - date_value.replace(day=1).isocalendar()[1] + 1)
 
 
-if __name__ == '__main__':
+def dates_test():
     today = datetime.datetime.now()
     print(today)
     # print(round(datetime.datetime.now().timestamp()))
-    print(today.strftime("%B")) # month name
+    print(today.strftime("%B"))  # month name
     # print(datetime.date.isocalendar())
     print(week_number_of_month(today))
 
+
+if __name__ == '__main__':
+    # dates_test()
+    str = "2019-05-20T10:07:44.537+00:00"
+    print(str[0:4])
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import functions_framework
 import pymongo
 import os
+import bson.json_util as json_util
 
 
 @functions_framework.http
@@ -11,13 +12,21 @@ def workouts_month(request):
         # request_json = request.get_json(silent=True)
         request_args = request.args
         month = request_args["month"]
+        year = request_args["year"]
         client = pymongo.MongoClient(os.getenv("DB_URL"))
         db = client['workouts']
         work_collection = db['workouts']
         # today = datetime.datetime.now()
-        workouts_cursor = work_collection.find({"month": month})
+        workouts_cursor = work_collection.find({"month": month}).sort("record", -1)
+            # .find({"month": month, "creation_date": {"$gt": year}}).sort("record", -1)
         workouts = []
         for work in workouts_cursor:
-            workouts.append(work)
-        return json.dumps(workouts)
+            # if work.get("creation_date")[-3] == year:
+            if work.get("creation_date")[0:4] == year:
+                workouts.append(work)
+        print(">>> workouts data: \n")
+        print(workouts)
+        # d = Headers()
+        # d.add('Content-Type', 'text/plain')
+        return json_util.dumps(workouts), 200, {'ContentType': 'application/json'}
     return "TEST"
